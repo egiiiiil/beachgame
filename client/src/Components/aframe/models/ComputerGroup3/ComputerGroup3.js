2cc0ccaf480772b242779772b1b067132f8c08d3
egiiiiil <79319021+egiiiiil@users.noreply.github.com>
2022-05-31 09:39:41 +0200

    work on navmesh

--- client/src/Components/aframe/models/ComputerGroup3/ComputerGroup3.js
+++ client/src/Components/aframe/models/ComputerGroup3/ComputerGroup3.js
@@ -3,7 +3,7 @@ import Table from '../Table/Table.js'
 
 const ComputerGroup3 = () => {
 	return (
-		<a-entity position="2.600 -0.656 8" rotation="0 18.521 0">
+		<a-entity position='2.600 -0.787 8' rotation='0 18.521 0'>
 			<Computer />
 			<Table />
 		</a-entity>
--- client/src/Components/aframe/models/Ocean/Ocean.js
+++ client/src/Components/aframe/models/Ocean/Ocean.js
@@ -4,9 +4,9 @@ const Ocean = () => {
 	return (
 		<a-gltf-model
 			src={OceanGLB}
-			animation="property: object3D.position.y; to: -12; dir: alternate; dur: 2000; loop: true"
-			position="0 -13 0"
-			scale="1 0.200 1"
+			animation='property: object3D.position.y; to: -11.822; dir: alternate; dur: 2000; loop: true'
+			position='0 -14.673 0'
+			scale='1 0.200 1'
 			shadow></a-gltf-model>
 	)
 }
